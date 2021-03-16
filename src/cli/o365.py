d241fcc6aff69347090de32cea027de4906090fb
Renato Teixeira <rena2damas@gmail.com>
2021-03-16 14:30:04 +0000

    Project restructure

--- src/cli/o365.py
+++ src/cli/o365.py
@@ -2,6 +2,8 @@ import click
 from flask import current_app
 from flask.cli import AppGroup
 from O365 import Account, MSOffice365Protocol
+from o365_notifications.base import O365Notification
+from o365_notifications.streaming.mailbox import O365MailBoxStreamingNotifications
 
 o365_cli = AppGroup(
     'o365',
@@ -46,28 +48,31 @@ def handle_incoming_email(mailbox=None, retries=0):
     account = authenticate(mailbox=mailbox, retries=retries)
     mailbox = account.mailbox()
 
-    mailbox_subscriber = MailBoxStreamingSubscription(parent=mailbox,
-                                                      change_type=Notification.ChangeType.CREATED.value)
+    mailbox_notifications = O365MailBoxStreamingNotifications(
+        parent=mailbox,
+        change_type=O365Notification.ChangeType.CREATED.value
+    )
 
     # Change Id alias to the real id for the 'RecipientsFilter' object
     sent_folder = mailbox.sent_folder()
     sent_folder = sent_folder.get_folder(folder_id=sent_folder.folder_id)
 
     # Filling the HPDA Support object
-    support = hpdasupport.HPDASupport(mailbox=mailbox) \
-        .subscriber(mailbox_subscriber) \
-        .jira(url=config['ATLASSIAN_URL'],
-              user=config['ATLASSIAN_USER'],
-              token=config['ATLASSIAN_API_TOKEN']) \
+    manager = hpdasupport.HPDASupport(mailbox=mailbox) \
+        .subscriber(mailbox_notifications) \
+        .jira(url=current_app.config['ATLASSIAN_URL'],
+              user=current_app.config['ATLASSIAN_USER'],
+              token=current_app.config['ATLASSIAN_API_TOKEN']) \
         .filters([
-        hpdasupport.SenderEmailDomainWhitelistedFilter(whitelisted_domains=config['EMAIL_WHITELISTED_DOMAINS']),
-        hpdasupport.SenderEmailBlacklistFilter(blacklist=config['EMAIL_BLACKLIST']),
-        hpdasupport.RecipientsFilter(recipient_reference=config['MAILBOX'], sent_folder=sent_folder),
+        hpdasupport.SenderEmailDomainWhitelistedFilter(
+            whitelisted_domains=current_app.config['EMAIL_WHITELISTED_DOMAINS']),
+        hpdasupport.SenderEmailBlacklistFilter(blacklist=current_app.config['EMAIL_BLACKLIST']),
+        hpdasupport.RecipientsFilter(recipient_reference=current_app.config['MAILBOX'], sent_folder=sent_folder),
         hpdasupport.ValidateMetadataFilter(),
         hpdasupport.JiraCommentNotificationFilter(mailbox=mailbox)
     ])
 
     # Start listening for incoming notifications...
-    support.listen_to_notifications(connection_timeout=config['CONNECTION_TIMEOUT_IN_MINUTES'],
-                                    keep_alive_interval=config['KEEP_ALIVE_NOTIFICATION_INTERVAL_IN_SECONDS'],
-                                    refresh_after_expire=True)
+    manager.notification_manager(connection_timeout=current_app.config['CONNECTION_TIMEOUT_IN_MINUTES'],
+                                 keep_alive_interval=current_app.config['KEEP_ALIVE_NOTIFICATION_INTERVAL_IN_SECONDS'],
+                                 refresh_after_expire=True)
