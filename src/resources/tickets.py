c89ced9a7ea9cf5f432a8a338d9bdec18311baa6
Renato Teixeira <rena2damas@gmail.com>
2021-04-22 14:04:02 +0100

    Define parameters

--- src/resources/tickets.py
+++ src/resources/tickets.py
@@ -33,35 +33,90 @@ class Tickets(Resource):
     # @tickets.marshal_list_with(issue, skip_none=True)
     # @tickets.response(200, 'Ok')
     # @tickets.response(400, 'Bad request')
-    @swag_from(definition=Issue)
+    @swag_from({
+        'parameters': [
+            {
+                'name': 'limit',
+                'description': 'results limit',
+                'in': 'query',
+                'default': 20,
+                'required': True
+            },
+            {
+                'name': 'boards',
+                'description': 'boards to fetch tickets from',
+                'in': 'query',
+                'schema': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'string',
+                        'enum': JiraService.supported_board_keys()
+                    },
+                },
+                'default': [current_app.config['JIRA_DEFAULT_BOARD']['key']],
+                'explode': False,
+                'required': True
+            },
+            {
+                'name': 'categories',
+                'description': 'categories that the ticket belongs to',
+                'in': 'query',
+                'schema': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'string',
+                        'enum': JiraService.supported_categories()
+                    },
+                },
+                'default': [current_app.config['JIRA_TICKET_LABEL_DEFAULT_CATEGORY']],
+                'explode': False,
+                'required': True
+            }
+        ],
+        'responses': {
+            200: {
+                'description': 'Ok'
+            }
+        }
+    })
     def get(self):
         """
         Get service tickets based on search criteria.
-        ---
-        parameters:
-            - name: limit
-              description: results limit
-              in: query
-              default: 20
-              required: true
-        responses:
-            200:
-                description: Ok
-                content:
-                    application/json:
-                        schema:
-                            type: array
-                            items:
-                                $ref: '#/components/schemas/Issue'
-            400:
-                description: Bad request
+        # ---
+        # parameters:
+        #     - name: limit
+        #       description: results limit
+        #       in: query
+        #       default: 20
+        #       required: true
+        #     - name: boards
+        #       description: boards to fetch tickets from
+        #       in: query
+        #       schema:
+        #           type: array
+        #           items:
+        #             type: string
+        #             enum: [1,2]
+        #       explode: false
+        #       required: true
+        # responses:
+        #     200:
+        #         description: Ok
+        #         content:
+        #             application/json:
+        #                 schema:
+        #                     type: array
+        #                     items:
+        #                         $ref: '#/components/schemas/Issue'
+        #     400:
+        #         description: Bad request
         """
-        from pprint import pprint
-        # pprint(swagger.get_apispecs())
         params = request.args.to_dict()
         limit = params.pop('limit', 20)
         boards = params.pop('boards', '').split(',')
         fields = params.pop('fields', '').split(',')
+
+        print(JiraService.supported_board_keys())
 
         return TicketService.find_by(limit=limit, boards=boards, fields=fields, **params)
 
@@ -92,7 +147,6 @@ class Tickets(Resource):
     #     #     ), 201
     #     # except jira.exceptions.JIRAError as ex:
     #     #     tickets.abort(400, ex.text)
-
 
 # @tickets.param('key', description='the ticket identifier')
 # @tickets.route('/<key>')
