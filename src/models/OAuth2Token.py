9ffc5aec2598c49d2c2063688202f6f8811ca818
Renato Teixeira <rena2damas@gmail.com>
2021-07-21 12:39:42 -0400

    Use database for token persistency

---
+++ src/models/OAuth2Token.py
@@ -0,0 +1,14 @@
+from src import db
+
+
+class OAuth2Token(db.Model):
+    __tablename__ = 'oauth2_tokens'
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String, nullable=False)
+    access_token = db.Column(db.String, nullable=False, unique=True)
+    refresh_token = db.Column(db.String, nullable=False, unique=True)
+    expires_in = db.Column(db.Integer, nullable=False)
+    ext_expires_in = db.Column(db.Integer, nullable=False)
+    expires_at = db.Column(db.Float, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
