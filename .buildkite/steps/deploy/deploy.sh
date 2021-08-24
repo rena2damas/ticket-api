abbae9dc42b7bc724c303e544b008bcfc4a7e5f5
Renato Teixeira <rena2damas@gmail.com>
2021-08-24 15:42:59 +0100

    Use kustomize to set image

--- .buildkite/steps/deploy/deploy.sh
+++ .buildkite/steps/deploy/deploy.sh
@@ -6,19 +6,29 @@ if ! kubectl get deployment --selector app=ticket-api ||
   ! kubectl get pod --selector app=ticket-bridge; then
   echo "--- :boom: Missing resources"
   exit 1
-else
-  # base location for services
-  basedir=".kustomization/components"
-  newImage="${REGISTRY}/${REGISTRY_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}"
-  (cd "${basedir}/" && kustomize edit set image "$newImage")
 
-  # restart api service
-  kustomize build "${basedir}/api/deployment.yaml" | kubectl apply -f -
-  kubectl logs --selector app=ticket-api --follow &
-  kubectl wait --for condition=running --timeout=300s pods/ticket-api
+# create kustomization context
+cwd=$(pwd)
+ctx=$(mktemp -dt tmp.deploy.XXXXXX)
+cd $ctx
 
-  # restart bridge service
-  kustomize build "${basedir}/bridge/pod.yaml" | kubectl apply -f -
-  kubectl logs --selector app=ticket-bridge --follow &
-  kubectl wait --for condition=running --timeout=300s pods/ticket-bridge
-fi
+basedir=".kustomization/components"
+newImage="${REGISTRY}/${REGISTRY_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}"
+
+# set kustomization file used to restart services
+kustomize create
+kustomize edit add resource "${cwd}/${basedir}/api/deployment.yaml"
+kustomize edit add resource "${cwd}/${basedir}/bridge/pod.yaml"
+kustomize edit set image "$newImage"
+
+# restart api & bridge service
+kustomize build . | kubectl replace -f -
+kubectl logs --selector app=ticket-api --follow &
+kubectl logs --selector app=ticket-bridge --follow &
+
+# wait service conclusion
+kubectl wait --for condition=running --timeout=300s pods/ticket-api
+kubectl wait --for condition=running --timeout=300s pods/ticket-bridge
+
+# cleanup
+rm -rf -- "$tmpdir"
