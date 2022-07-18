64f4cb4d18ca7651b0344ba52d1a0db17b6aa63e
Renato Teixeira <rena2damas@gmail.com>
2022-07-18 13:36:43 +0100

    Rewrite dict as literal

--- src/schemas/deserializers/TicketSearchCriteria.py
+++ src/schemas/deserializers/TicketSearchCriteria.py
@@ -6,25 +6,25 @@ from src.services.jira import JiraSvc
 class TicketSearchCriteriaSchema(Schema):
     boards = fields.List(
         fields.String(validate=validate.OneOf(JiraSvc.supported_board_keys())),
-        metadata=dict(description="boards to fetch tickets from"),
+        metadata={"description": "boards to fetch tickets from"},
     )
     categories = fields.List(
         fields.String(validate=validate.OneOf(JiraSvc.supported_categories())),
-        metadata=dict(description="categories the tickets belongs to"),
+        metadata={"description": "categories the tickets belongs to"},
     )
     reporter = fields.Email(metadata=dict(description="ticket reporter email"))
     assignee = fields.Email(
-        metadata=dict(description="user whose ticket is assigned to")
+        metadata={"description": "user whose ticket is assigned to"}
     )
     status = fields.String(
-        metadata=dict(description="name of the current ticket status")
+        metadata={"description": "name of the current ticket status"}
     )
     watcher = fields.Email(metadata=dict(description="tickets user has subscribed to"))
     q = fields.String(metadata=dict(description="search for text occurrences"))
     fields_ = fields.List(
         fields.String(validate=validate.OneOf(JiraSvc.supported_fields())),
         data_key="fields",
-        metadata=dict(description="additional fields to include in the results"),
+        metadata={"description": "additional fields to include in the results"},
     )
     limit = fields.Integer(
         load_default=20, metadata=dict(description="tickets user has subscribed to")
@@ -32,5 +32,5 @@ class TicketSearchCriteriaSchema(Schema):
     sort = fields.String(
         validate=validate.OneOf(["created"]),
         load_default="created",
-        metadata=dict(description="sort tickets by"),
+        metadata={"description": "sort tickets by"},
     )
