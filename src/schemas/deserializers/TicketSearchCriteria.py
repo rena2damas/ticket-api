22bad020ff99f237dad8ade821f2409bd1605b3b
Renato Teixeira <rena2damas@gmail.com>
2022-07-18 13:38:41 +0100

    Rewrite dict as literal

--- src/schemas/deserializers/TicketSearchCriteria.py
+++ src/schemas/deserializers/TicketSearchCriteria.py
@@ -12,22 +12,22 @@ class TicketSearchCriteriaSchema(Schema):
         fields.String(validate=validate.OneOf(JiraSvc.supported_categories())),
         metadata={"description": "categories the tickets belongs to"},
     )
-    reporter = fields.Email(metadata=dict(description="ticket reporter email"))
+    reporter = fields.Email(metadata={"description": "ticket reporter email"})
     assignee = fields.Email(
         metadata={"description": "user whose ticket is assigned to"}
     )
     status = fields.String(
         metadata={"description": "name of the current ticket status"}
     )
-    watcher = fields.Email(metadata=dict(description="tickets user has subscribed to"))
-    q = fields.String(metadata=dict(description="search for text occurrences"))
+    watcher = fields.Email(metadata={"description": "tickets user has subscribed to"})
+    q = fields.String(metadata={"description": "search for text occurrences"})
     fields_ = fields.List(
         fields.String(validate=validate.OneOf(JiraSvc.supported_fields())),
         data_key="fields",
         metadata={"description": "additional fields to include in the results"},
     )
     limit = fields.Integer(
-        load_default=20, metadata=dict(description="tickets user has subscribed to")
+        load_default=20, metadata={"description": "tickets user has subscribed to"}
     )
     sort = fields.String(
         validate=validate.OneOf(["created"]),
